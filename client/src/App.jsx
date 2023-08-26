784c04124181f5fa7562d9f84edc7f8c31734bc3
Akram-Star-Coder <elbasria31@gmail.com>
2023-08-26 01:01:44 +0100

    sdf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Pages/Home';
+import Reset from './Pages/Reset';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Forgot from './Pages/Forgot';
+import P404 from './Pages/P404';
+import OTP from './Pages/OTP';
+import Profile from './Pages/Profile';
+import PostPage from './Pages/PostPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const token = localStorage.getItem('token');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={  token ? <Navigate to='/'/> : <Register />  } />
+          <Route path='/login' element={  token ? <Navigate to='/'/> : <Login />  } />
+          <Route path='/forgot-password' element={  token ? <Navigate to='/'/> : <Forgot />  } />
+          <Route path='/otp' element={  <OTP />  } />
+          <Route path='/reset-password' element={   <Reset /> } />
+          <Route path='*' element={<P404 />} />
+          <Route path='/profile/:id' element={<Profile />} />
+          <Route path='/post/:id' element={<PostPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
